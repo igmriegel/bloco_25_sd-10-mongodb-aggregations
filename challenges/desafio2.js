e7381a6ae2d9397c9aca4eb1e35a434c856f558a
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-08-23 16:06:42 -0300

    Desafio 02 concluido

---
+++ challenges/desafio2.js
@@ -0,0 +1,17 @@
+db.movies.aggregate([
+  { $match: {
+    $and: [
+      { "imdb.rating": { $gte: 7 } },
+      { genres: { $nin: ["Crime", "Horror"] } },
+      { rated: { $in: ["PG", "G"] } },
+      { languages: { $all: ["English", "Spanish"] } },
+    ] } },
+  { $project: {
+    _id: false,
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year",
+  } },
+]);
